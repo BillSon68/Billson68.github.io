e3d0b4b39fdbe71c0a01e8f27187edd14e721fc8
BillSon68 <Billson68@gmail.com>
2020-06-21 21:51:14 +0800

    init prj file

---
+++ precache-manifest.2b7c0ae7b8f9a81b29319b169ddd9dd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e3711828e30b005f84",
+    "url": "/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "fe2f5831ac85493f21e1",
+    "url": "/css/chunk-vendors.edff094c.css"
+  },
+  {
+    "revision": "8bf6c05119fca579511fadae29225f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4eb994809ff796c78f8",
+    "url": "/js/about.784cbeee.js"
+  },
+  {
+    "revision": "f7e3711828e30b005f84",
+    "url": "/js/app.4a7a7663.js"
+  },
+  {
+    "revision": "fe2f5831ac85493f21e1",
+    "url": "/js/chunk-vendors.4bb0edf0.js"
+  },
+  {
+    "revision": "c5ac4dd8cb8b0637a69e8e8853fbeb43",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
